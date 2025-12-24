3d9ab90d19c97a7c7ab754e44e8f60701da1dfa7
Tom <stokes.thomas@gmail.com>
2025-12-24 11:24:49 +0000

    Removed obsolete console prints

--- main.py
+++ main.py
@@ -14,8 +14,6 @@ from shot import *
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     # initialise pygame and set display
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
